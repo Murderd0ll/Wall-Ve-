ca78cce5181013efd92b16c03961921a4f2ba170
Morán <brayanmoranrivera@hotmail.com>
2024-04-10 00:52:29 -0700

    *Página de ventas de administrador, falta la parte de abajo donde dice en que paso va

--- Wall-VE-main/Admin_Ventas.php
+++ Wall-VE-main/Admin_Ventas.php
@@ -5,7 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
            <!-- Fuentes  -->
    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
    <link rel="stylesheet" href="css/estilos.css"/>
+   <link rel="stylesheet" href="css/tarjetas.css">
+
+  
+
+
     <title>Venta de energía | Wall-VE </title>
 </head>
 <body>
@@ -16,7 +25,7 @@
     </header>
    
     <div id="subtitulo">
-        <p>Seleccione una opción para comenzar.</p>
+        <p id="seleccione">Seleccione una opción para comenzar.</p>
     </div>
 
 
@@ -33,7 +42,7 @@
                 </li>
 
                 <li class="activo">
-                    <a href="Admin_Ventas.html">
+                    <a href="Admin_Ventas.php">
                         <i class="fa-solid fa-dollar-sign"></i>
                         <span>Ventas</span>
                     </a>
@@ -82,6 +91,49 @@
                     </a>
             </ul>
     </div>
-    
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+            
+            <div class="card-body border border-primary" >
+                <h5 class="card-title ">Cantidad en watts</h5>
+                <p class="card-text ">$XX<span class="pWatt">/Watt</span></p>
+                <a href="#" class="btn btn-primary">Seleccionar</a>
+
+            </div>
+
+        </div>
+        <div class="col-sm">
+            
+            <div class="card-body border border-primary" >
+                <h5 class="card-title">Cantidad en watts</h5>
+                <p class="card-text">$XX<span class="pWatt">/Watt</span></p>
+                <a href="#" class="btn btn-primary">Seleccionar</a>
+
+            </div>
+
+        </div>
+            <div class="col-sm">
+            
+            <div class="card-body border border-primary" id="completa" >
+                <h5 class="card-title" id="cCompleta">Carga completa</h5>
+                
+                <a href="#" class="btn btn-primary">Seleccionar</a>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 </html>
