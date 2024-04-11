618b8ab70a978497d0b9b94ac6a93c6d2fd38d9a
Murderd0ll <moran.reyna.6mt@gmail.com>
2024-04-11 04:49:00 -0700

    **Se hicieron varias modificaciones al css de estilos.css y un par a la de style.css. **Se terminó de cambiar los inputs a LIST *Se agregaron algunos title a los input en Admin_Editar. (Faltan en Admin_Agregar) **Se modificaron botones en Admin_Agregar y Admin_Editar **Se agregó el ojito en las mencionadas anteriormente + el login, pero en el login se desacomodó el logo.
    ****FALTA: -Evitar que se guarden usuarios vacios.
    -Acomodar el ojito y el logo del login.
    -Dropdown de la izquierda. :(

--- Wall-VE-main/Login.php
+++ Wall-VE-main/Login.php
@@ -1,54 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 
     <div class="division left">
-        
-    <h1>Inicia sesión</h1>
-    
-        <img class ="logo" src="./img/logo.svg" srcset="">
-        
+
+        <h1>Inicia sesión</h1>
+
+        <img class="logo" src="./img/logo.svg" srcset="">
+
         <div class="centrado">
-            
-        
-            
+
+
+
 
             <form action="LoginComp.php" method="POST">
-            <?php if(isset($_GET['error'])) {?>
-                <p id="errorMsj"><?php echo $_GET['error']?></p>
-            <?php } ?>
-                    <div class="tbox">
-                        <label for="nombre" id="nombrelbl">Usuario</label>
-                        <input type="text" name="idloginEmp" placeholder="ejemplo.email@gmail.com">
+                <?php if (isset($_GET['error'])) { ?>
+                    <p id="errorMsj"><?php echo $_GET['error'] ?></p>
+                <?php } ?>
+                <div class="tbox">
+                    <label for="nombre" id="nombrelbl">Usuario</label>
+                    <input type="text" name="idloginEmp" placeholder="ejemplo.email@gmail.com">
+                </div>
+
+                <div class="tbox">
+                    <div class="tboxpass">
+                    <label for="password" id="passwordlbl">Contraseña</label>
+                            <div class="tboxInput">
+                                <input type="password" name="passEmp" placeholder="Ingrese su contraseña" id="password">
+                                <img class="eyeicon" src="img/eyeClosed.png" id="eyeicon">
+                            </div>
                     </div>
-                    <div class="tbox">
-                        <label for="password" id="passwordlbl" >Contraseña</label>
-                        <input type="password" name="passEmp" placeholder="Ingrese su contraseña">
-                    </div>
-                    <button class="btn">Entrar</button>
+                    
+                </div>
+
+                <script>
+                    let eyeicon = document.getElementById("eyeicon");
+                    let password = document.getElementById("password");
+
+                    eyeicon.onclick = function() {
+                        if (password.type == "password") {
+                            password.type = "text";
+                            eyeicon.src = "img/eyeOpen.png"
+                        } else {
+                            password.type = "password";
+                            eyeicon.src = "img/eyeClosed.png"
+                        }
+                    }
+                </script>
+
+
+
+                <button class="btn">Entrar</button>
             </form>
         </div>
-      </div>
-      
-      <div class="division right">
+    </div>
+
+    <div class="division right">
         <div class="centrado">
-          <img src="./img/loginbien.jpeg" alt="Avatar man">
-          
+            <img src="./img/loginbien.jpeg" alt="Avatar man">
         </div>
-      </div>
-
-
-  
-
-
-
-
+    </div>
 </body>
 </html>
