6051769dc115ab21b5ccb041c42956b7aef24e54
Morán <brayanmoranrivera@hotmail.com>
2024-04-21 14:12:22 -0700

    *Ahora tiene validación en caso de que no exista el ticket

--- Wall-VE-main/Empleado_GenRepTicketIndvdl.php
+++ Wall-VE-main/Empleado_GenRepTicketIndvdl.php
@@ -1,11 +1,13 @@
 <?php
+
 if (session_status() == PHP_SESSION_NONE) {
-  session_start();
+    session_start();
 }
 if(empty($_SESSION['user'])){
     header('location:Login.php');
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -39,9 +41,9 @@ if(empty($_SESSION['user'])){
             alert("Por favor ingrese un folio.");
             return false;
         }
-
+        return true;
         // Llama a la función mandarFolio y retorna su resultado
-        return mandarFolio(folioVenta);
+      //  return mandarFolio(folioVenta);
     }
 
     function mandarFolio(folio) {
@@ -99,12 +101,52 @@ if(empty($_SESSION['user'])){
 
             </ul>
     </div>
+    <?php 
+    if (isset($_POST['busqueda'])) {
+        
+        include("connection/conexion.php");
+
+        $valorABuscar = $_POST['folioVenta'];
+        $sql = "SELECT COUNT(*) FROM tventa WHERE folioVenta = '$valorABuscar'";
+       
+
+        // Ejecutar la consulta
+        $resultado = mysqli_query($conexion, $sql);
+
+
+        // Verificar si el valor existe
+        if ($resultado) { 
+            $row = mysqli_fetch_assoc($resultado);
+    
+            // Obtener el valor de count
+            $count = $row['COUNT(*)'];
+            if ($count > 0) {
+                
+
+                
+                //echo "El valor '$valorABuscar' existe en la tabla. Total de registros encontrados: $count";
+                header('location: Empleado_MostrarTicket.php?folio='.$valorABuscar.'');
+
+            } else {
+                //echo "El valor '$valorABuscar' no existe en la tabla.";
+                
+                echo "<script language ='JavaScript'>
+                    alert('Error: El folio no existe, intente con otro folio por favor.');
+                    location.assign('Empleado_GenRepTicketIndvdl.php');
+                    </script>";
+            }
+        } else {
+            echo "Error al ejecutar la consulta.";
+        }
+        
+    }
+    ?>
 
 
 
     <div class="formCarga">
     <h3 class="ventaPor">Ingrese el número de folio del ticket a buscar</h2>
-        <form class="row g-3" autocomplete="off" onsubmit="return validarFormulario()"  >
+        <form class="row g-3" autocomplete="off" onsubmit="return validarFormulario()" method = "post" >
             <div class="col-md-6">
                 <label for="inputFolioBusqueda" class="form-label">Folio</label>
                 <input type="number" class="form-control" id="inputFolioBusqueda" name="folioVenta" placeholder="Ingrese un folio aqui" >
