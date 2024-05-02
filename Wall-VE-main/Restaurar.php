35e672623c41742aa5e8a3757f058ba1d7f45cbc
Murderd0ll <moran.reyna.6mt@gmail.com>
2024-05-02 16:31:27 -0700

    **Se agregó el Session a copias de seguridad y restaurar para poder mostrar el titulo.

--- Wall-VE-main/Restaurar.php
+++ Wall-VE-main/Restaurar.php
@@ -1,3 +1,15 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if (empty($_SESSION['user'])) {
+    header('location:Login.php');
+}
+?>
+<?php
+include("connection/conexion.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
